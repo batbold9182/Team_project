817d073d9c7c8336c009e69af60ab865d5aed771
Batbold Samdan <samdaba_aehit@students.vizja.pl>
2026-03-12 09:53:11 +0100

    mongoDB holbosn server setup bolsn

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API check ajilj baina"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
